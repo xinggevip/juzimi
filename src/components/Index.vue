1efd6b96de2772d0120136bc4de155fc2e91956d
xinggevip <1511844263@qq.com>
2019-11-10 10:55:18 +0800

    首页展示所有句子已完成

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -7,23 +7,24 @@
         <div class="mdui-col-md-9">
           <div class="mdui-row">
             
-            <div class="mdui-col-md-12" v-for="customer in customers" :key="customer.id">
+            <div class="mdui-col-md-12" v-for="sen in sentenceList" :key="sen.sentenceId" >
               <div class="list mdui-clearfix mdui-hoverable">
                 <div class="mdui-chip">
                   <span class="mdui-chip-icon mdui-color-blue">
                     <i class="mdui-icon material-icons">face</i>
                   </span>
-                  <span class="mdui-chip-title">{{customer.id}}</span>
+                  <span class="mdui-chip-title">{{sen.userId}}</span>
                 </div>
                 <button class="mdui-btn mdui-btn-icon mdui-ripple mdui-float-right">
                   <i class="mdui-icon material-icons">favorite_border</i>
                 </button>
-                <p style="margin-bottom:20px;">{{customer.juzi}}</p>
+                <p style="margin-bottom:23px;">{{sen.sentenceTxt}}</p>
+                <span class="mdui-float-left" style="font-size:13px;color:#ccc">{{sen.createDate}}</span>
                 <span class="mdui-float-right mdui-text-color-blue-900">
                   <a href="javascript:;" class="mdui-text-color-black-icon-disabled">
-                    <span style="letter-spacing:-3px;">———</span>&nbsp;&nbsp;{{customer.writer}}
+                    <span style="letter-spacing:-3px;">———</span>&nbsp;&nbsp;{{sen.authorName}}
                   </a> &nbsp;&nbsp;
-                  <a href="javascript:;" class="mdui-text-color-black-secondary">《{{customer.from}}》</a>
+                  <a href="javascript:;" class="mdui-text-color-black-secondary">《{{sen.albumName}}》</a>
                 </span>
               </div>
             </div>
@@ -33,7 +34,10 @@
           </div>
 
         <div class="mdui-row">
-            <button v-if="!load" class="mdui-btn mdui-btn-raised mdui-ripple mdui-center" v-on:click="onload()">点击加载更多</button>
+            <!-- 没有数据了 -->
+          <p v-if="!next" class="nulldata">——我是有底线的——</p>
+
+            <button v-if="next && !load" class="mdui-btn mdui-btn-raised mdui-ripple mdui-center" v-on:click="onload()" >点击加载更多</button>
 
             <!-- load -->
             <div v-if="load" class="mdui-spinner mdui-center"><div class="mdui-spinner-layer "><div class="mdui-spinner-circle-clipper mdui-spinner-left"><div class="mdui-spinner-circle"></div></div><div class="mdui-spinner-gap-patch"><div class="mdui-spinner-circle"></div></div><div class="mdui-spinner-circle-clipper mdui-spinner-right"><div class="mdui-spinner-circle"></div></div></div></div>
@@ -92,9 +96,15 @@ import Footer from "@/components/Footer.vue";
 export default {
   data() {
     return {
-      customers: [],
+      next:false,
+      sentenceList:[],
       more:[],
-      load:true
+      load:true,
+      SentenceRequestByAuto:{
+        albumId:-1,
+        pageNum:1,
+        pageSize:10
+      },
     };
   },
   name: "home",
@@ -109,47 +119,156 @@ export default {
 
   methods: {
     // Get数据
-    fetchCustomers() {
-      this.$http.get("http://localhost:3000/juzi").then(response => {
+    fetchSentence() {
+      // this.$http.get("http://localhost:3000/juzi").then(response => {
+      //   // 响应成功回调
+      //   // 打印获取到的数据
+      //   // console.log(response);
+      //   // 把数据赋值给customers
+      //   this.customers = response.data;
+      //   console.log(this.customers);
+      //   this.load = false;
+      // }),
+      //   function(response) {
+      //     // 响应错误回调
+      //   };
+
+
+      this.$http.post("/api/selectallsentence",this.SentenceRequestByAuto,{
+        headers: {
+            'Content-Type':'application/json;charset=UTF-8'
+        }
+
+      }).then(response => {
         // 响应成功回调
-        // 打印获取到的数据
-        // console.log(response);
-        // 把数据赋值给customers
-        this.customers = response.data;
-        console.log(this.customers);
+        console.log(response.data);
+        // 遍历改日期
+        Date.prototype.toLocaleString = function() {
+        // 补0   例如 2018/7/10 14:7:2  补完后为 2018/07/10 14:07:02
+        function addZero(num) {
+            if(num<10)
+                return "0" + num;
+            return num;
+        }
+        // 按自定义拼接格式返回
+            return this.getFullYear() + "年" + addZero(this.getMonth() + 1) + "月" + addZero(this.getDate()) + "日"
+            //  + addZero(this.getHours()) + ":" + addZero(this.getMinutes()) + ":" + addZero(this.getSeconds())
+             ;
+        };
+
+        (response.data.sentenceProList).forEach((item, index) => {
+            // item.albumPicture = this.$global.globalPictureUrl + item.albumPicture;
+            var date = new Date(item.createDate);
+            item.createDate = date.toLocaleString();
+            //在这 改日期  
+            // console.log(item);
+            this.sentenceList.push(item);
+          })
+
+
+
+        // this.sentenceList = response.data.sentenceList;
+        this.next = response.data.next;
+        
+
         this.load = false;
       }),
         function(response) {
           // 响应错误回调
         };
+
+
     },
     onload:function(){
       // alert("hjk");
       this.load = true;
 
-      // 获取数据
-      this.$http.get("http://localhost:3000/juzimore").then(response => {
-        // 响应成功回调
-        // 打印获取到的数据
-        console.log(response);
-        // 把数据赋值给customers
-        this.more = response.data;
-        console.log(this.more);
+      // // 获取数据
+      // this.$http.get("http://localhost:3000/juzimore").then(response => {
+      //   // 响应成功回调
+      //   // 打印获取到的数据
+      //   console.log(response);
+      //   // 把数据赋值给customers
+      //   this.more = response.data;
+      //   console.log(this.more);
 
-        // 遍历数据
-        this.more.forEach((item, index) => {
+      //   // 遍历数据
+      //   this.more.forEach((item, index) => {
         
-        this.customers.push(item);
-        this.load = false;
-        })
-      }),
-        function(response) {
-          // 响应错误回调
+      //   this.customers.push(item);
+      //   this.load = false;
+      //   })
+      // }),
+      //   function(response) {
+      //     // 响应错误回调
+      //   };
+
+
+
+      this.SentenceRequestByAuto.pageNum = this.SentenceRequestByAuto.pageNum + 1;
+        console.log(this.SentenceRequestByAuto.pageNum);
+        this.$http.post("/api/selectallsentence",this.SentenceRequestByAuto,{
+        headers: {
+            'Content-Type':'application/json;charset=UTF-8'
+        }
+
+        }).then(response => {
+          // 响应成功回调
+          // 打印获取到的数据
+          console.log(response.data);
+
+          Date.prototype.toLocaleString = function() {
+        // 补0   例如 2018/7/10 14:7:2  补完后为 2018/07/10 14:07:02
+        function addZero(num) {
+            if(num<10)
+                return "0" + num;
+            return num;
+        }
+        // 按自定义拼接格式返回
+            return this.getFullYear() + "年" + addZero(this.getMonth() + 1) + "月" + addZero(this.getDate()) + "日"
+            //  + addZero(this.getHours()) + ":" + addZero(this.getMinutes()) + ":" + addZero(this.getSeconds())
+             ;
         };
+
+          this.next = response.data.next;
+          let arr = response.data.sentenceProList;
+          this.load = false;
+          arr.forEach((item, index) => {
+            // item.albumPicture = this.$global.globalPictureUrl + item.albumPicture;
+
+            //在这 改日期  
+
+            var date = new Date(item.createDate);
+            item.createDate = date.toLocaleString();
+
+            // console.log(item);
+            this.sentenceList.push(item);
+          })
+
+
+        }),
+          function(response) {
+            // 响应错误回调
+            alert("未知错误");
+            this.load = false;
+          };
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
   },
   created() {
-    this.fetchCustomers();
+    this.fetchSentence();
   }
 };
 </script>
